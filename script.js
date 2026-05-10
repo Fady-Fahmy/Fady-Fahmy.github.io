a82ed34dabcf680e7c42883d0ef6ca4a551948c6
Fady Fahmy <manomano694@gmail.com>
2026-05-10 14:42:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
 // Hamburger menu
 const navToggle = document.querySelector('.nav-toggle');
 const navEl = document.querySelector('nav');
