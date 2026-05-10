51ccb368c92452a45113b49ebe9a470434f82a90
Fady Fahmy <manomano694@gmail.com>
2026-05-10 14:42:03 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
